e8a507f9cbd4748cb044cfee6f2e24703c3ee2bf
rachit74 <rachithooda09@gmail.com>
2025-09-18 13:48:50 +0530

    accepting client connections

---
+++ server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	/*
+		net.Listen() takes 2 args, type of conn and port number
+	*/
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer ln.Close() // defer used to close connection
+
+	fmt.Println("Server is listening on port 8080")
+	/*
+		A defer statement defers the execution of a function until the surrounding function returns.
+		defer ln.Close() will execute after everything else in the main function
+		closing and cleaning after all communication
+	*/
+
+	// Accepting client connections
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		go handleClient(conn)
+
+	}
+
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New Client connection", conn.RemoteAddr())
+
+	// Read and process data from the client
+	// ...
+
+	// Write data back to the client
+	// ...
+}
